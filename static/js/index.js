6061780dddf471df4ea081db1cb25a8216a2e54a
tijothomas <tijothomas366@gmail.com>
2025-03-29 21:41:07 +0530

    Initial commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,30 @@
-var chat = document.getElementById("chat");
-chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: 'https://block-new-wkus.onrender.com',
+    methods: ['GET', 'POST'],
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('A user connected')
+
+  socket.on('message', (data) => {
+    console.log('Message received:', data)
+    io.emit('message', data) // Broadcast to all clients
+  })
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected')
+  })
+})
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,43 @@
-$(function(){
-	$('.chat-area > .chat-list').jScrollPane({
-		mouseWheelSpeed: 30
-	});
-});
+$(function () {
+  // Initialize custom scrollbar
+  $('.chat-area > .chat-list').jScrollPane({
+    mouseWheelSpeed: 30,
+  })
+
+  // Connect to the WebSocket server
+  const socket = io('https://block-new-wkus.onrender.com')
+
+  // Function to update chat scroll
+  function updateChatScroll() {
+    var chat = $('.chat-area > .chat-list')
+    chat.data('jsp').scrollToBottom()
+  }
+
+  // Receive messages from the server
+  socket.on('message', (data) => {
+    const chatList = $('.chat-area > .chat-list > ul')
+    chatList.append(`<li>${data}</li>`)
+
+    // Refresh jScrollPane and scroll to the bottom
+    $('.chat-area > .chat-list').jScrollPane().data('jsp').scrollToBottom()
+  })
+
+  // Send message when button is clicked
+  $('#sendButton').on('click', function () {
+    const messageInput = $('#messageInput')
+    const message = messageInput.val().trim()
+
+    if (message !== '') {
+      socket.emit('message', message)
+      messageInput.val('') // Clear input field
+    }
+  })
+
+  // Send message when Enter key is pressed
+  $('#messageInput').on('keypress', function (e) {
+    if (e.which === 13) {
+      // Enter key
+      $('#sendButton').click()
+    }
+  })
+})
